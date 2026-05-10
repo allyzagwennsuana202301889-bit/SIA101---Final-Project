dc68f0aed46f45463a6cfdeff8383d2dc961c720
allyzagwennsuana202301889-bit <allyzagwennsuana.202301889@gmail.com>
2026-05-10 21:10:53 +0800

    initialized

--- index.js
+++ index.js
@@ -1,3 +1,67 @@
-// run `node index.js` in the terminal
+const express = require("express");
+const cors = require("cors");
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+/*
+===================================
+TEST ROUTE
+===================================
+*/
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "Notification Service Running"
+    });
+});
+
+/*
+===================================
+SEND NOTIFICATION
+===================================
+*/
+app.post("/notify", (req, res) => {
+
+    const { userId, title, message } = req.body;
+
+    // Validation
+    if (!userId || !title || !message) {
+        return res.status(400).json({
+            success: false,
+            message: "Missing required fields"
+        });
+    }
+
+    // Fake notification storage
+    const notification = {
+        userId,
+        title,
+        message,
+        createdAt: new Date()
+    };
+
+    console.log("NEW NOTIFICATION:");
+    console.log(notification);
+
+    // Response
+    res.json({
+        success: true,
+        message: "Notification sent successfully",
+        data: notification
+    });
+
+});
+
+/*
+===================================
+PORT
+===================================
+*/
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
